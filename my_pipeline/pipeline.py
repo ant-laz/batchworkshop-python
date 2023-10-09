0654f1770529dd2e597155a6526b01d31eca8fc5
Anthony Lazzaro <alazzaro@google.com>
2023-10-09 09:16:13 +0100

    adds clearer task instructions

--- my_pipeline/pipeline.py
+++ my_pipeline/pipeline.py
@@ -27,11 +27,16 @@ from apache_beam import PCollection
 
 class ExtractSpeech(beam.DoFn):
    
-   # element is a csv record
+   # input: element is a csv record
+   # output: just the text field of the csv records
    # example 1 speech filed has escape character enclosed in doulbe quotes '"'
    # 1,1,Albus Dumbledore,"I should have known you would be here, Professor McGonagall."
+   # example 1 output: 
+   # 'I should have known you would be here, Professor McGonagall.'
    # example 2 speech field has no escape character
    # 5,1,Albus Dumbledore,Hagrid is bringing him.
+   # example 2 output:
+   # 'Hagrid is bringing him.'
    # need to handle both cases, extract speech field for all lines
    def process(self, element: str) -> List[str]:
     speech_field = None
@@ -47,6 +52,12 @@ class ExtractSpeech(beam.DoFn):
 
 class SpeechToWords(beam.DoFn):
 
+    # input: string of text
+    # output: list of strings, each of word of the inputted text
+    # example input 1: 
+    # 'Hagrid is bringing him.'
+    # example output 1:
+    # [ 'Hagrid', 'is', 'bringing', 'him.' ]
     def process(self, element: str) -> List[str]:
         return element.split()
 
@@ -56,6 +67,12 @@ class SpeechToWords(beam.DoFn):
 
 class SanitizeWords(beam.DoFn):
 
+    # input: list of words
+    # output: list of clean up words with characters "," & "." removed. And lower case.
+    # exampe input: 
+    # ['Hagrid', 'is', 'bringing', 'him.']
+    # example output:
+    # ['hagrid', 'is', 'bringing', 'him']
     def process(self, element: str) -> List[str]:
         word = element
         word = word.lower()
@@ -68,7 +85,13 @@ class SanitizeWords(beam.DoFn):
 ########################################################################################
 
 class CountWordFrequency(beam.PTransform):
- 
+
+    # intput: a list of words as strings
+    # output: a list of tuple, each tuple looks like (word, count_of_word_occuring)
+    # example input: 
+    # ['harry', 'harry', 'harry', 'ron', 'ron', 'magic']
+    # example output: 
+    # [('harry',3), ('ron',2), ('magic',1)]
     def expand(self, pcoll: PCollection[str]) -> PCollection[Tuple[str, int]]:
         result = pcoll | "Count" >> beam.combiners.Count.PerElement()
         return result
@@ -78,7 +101,17 @@ class CountWordFrequency(beam.PTransform):
 ########################################################################################
 
 class TopWords(beam.PTransform):
- 
+
+    # input is a list of tuples, each looks like (word, count_of_word_occuring)
+    # output is the top 10 most frequently occuring words, as tuples (word, count)
+    # example input
+    # [('harry',100), ('voldermort',99), ('draco',98), ('ron',97),
+    #  ('dobby',96),  ('dumbeldore',95), ('snape',94), ('hagrid',93),
+    #  ('luna',92),   ('hedwig',91),     ('ginny',90), ('bellatrix',89) ]
+    # example output
+    # [('harry',100), ('voldermort',99), ('draco',98), ('ron',97),
+    #  ('dobby',96),  ('dumbeldore',95), ('snape',94), ('hagrid',93),
+    #  ('luna',92),   ('hedwig',91)]
     def expand(self, pcoll: PCollection[Tuple[str, int]]
     ) -> PCollection[List[Tuple[str, int]]]:
         result= pcoll | "Rank" >> beam.combiners.Top.Of(10,key=lambda t: t[1])   
@@ -90,6 +123,12 @@ class TopWords(beam.PTransform):
 
 class Prettify(beam.DoFn):
 
+    # input: a list of tuples, (word, count_of_word_occuring)
+    # output: a list of 1 string. Format: word,count\nword,count\nword,count
+    # example input:
+    # [('harry',100), ('voldermort',99), ('draco',98)]
+    # example output: 
+    # ["harry,100\nvoldermort,99\ndraco,98\n"]
     def process(self, element: List[Tuple[str, int]]) -> List[str]:
         pretty_str = ""
         for t in element:
@@ -101,7 +140,18 @@ class Prettify(beam.DoFn):
 ########################################################################################
 
 class FrequentWords(beam.PTransform):
- 
+
+    # input: pcoll is a PCollection of CSV records, each in a string format
+    # output: list of a single string. format: word,count\nword,count\nword,count
+    # example input
+    # [
+    #            '1,1,Albus Dumbledore,"harry Harry harry Harry',
+    #            '2,2,Albus Dumbledore,"potter, potter potter."',
+    #            '3,3,Albus Dumbledore,"magic, magic"',
+    #            '4,4,Albus Dumbledore,"hogwarts"',        
+    # ]
+    # example output
+    # [ "harry,4\npotter,3\nmagic,2\nhogwarts,1\n"]     
     def expand(self, pcoll: PCollection[str]) -> PCollection[str]:
         speech: PCollection[str] = pcoll | "speech" >> beam.ParDo(ExtractSpeech())
         words: PCollection[str] = speech | "words" >> beam.ParDo(SpeechToWords())
